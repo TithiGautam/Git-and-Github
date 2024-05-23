315c882365b4949201b6968c258e3196789edf89
Tithi Gautam <tithigautam@gmail.com>
2024-05-23 16:37:36 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html","./test/*.html"],  // bs yaha rakha kr ki jo file bnai hai vo yaha se 
-  //accessible hai age koi folder bna kr usme add kre toh add that here like ./folder_name/*.html ok
-  theme: {//bs ye / se ho jata h ky yha pe apn ne nhi likha h n koi folder name 
-    //han kyuki abhi koi folder nh hai na
+  content: ["./*.html","./test/*.html"],  
+  
+  theme: {
+
     extend: {},
   },
   plugins: [],
